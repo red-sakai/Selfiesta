f0d07082b0f0f5f8f2a9b71c6e6559dba3434ce6
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-07-08 12:04:20 +0800

    feat: add placeholders for contact and capture

---
+++ selfiesta/src/app/capture/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+const capture = () => {
+  return (
+    <div>
+      {/* Header */}
+      <header className="bg-[#9B8DD2] px-8 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          { /* proper way of initializing an image in next.js */}
+          <Image src="/selfiesta text middle with logo.PNG" 
+          alt="Selfiesta Logo" 
+          width={190} 
+          height={190} />
+
+        </div>
+        <nav className="flex gap-8">
+          {/* link is a special component of next.js for client side navigation, it's just like html href */}
+          <Link
+            href="/"
+            className="rounded-full px-5 py-1.5 text-lg font-medium text-white/80 hover:text-white transition"
+          >
+            Home
+          </Link>
+          <Link
+            href="/contact"
+            className="rounded-full px-5 py-1.5 text-lg font-medium text-white/80 hover:text-white transition"
+          >
+            Contact
+          </Link>
+          <Link
+            href="/capture"
+            className={cn(
+              "rounded-full px-5 py-1.5 text-lg font-medium text-white",
+              "bg-[#6B5CA5]"
+            )}
+          >
+            Capture
+          </Link>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default capture
